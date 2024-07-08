8be7a3a3e0f42e5402d05a4202eb447fcf693268
Jeremy Carder <jeremy.carder11@outlook.com>
2024-07-07 23:32:32 -0700

    Added icons/images

--- client/src/Pages/Splash.tsx
+++ client/src/Pages/Splash.tsx
@@ -1,8 +1,10 @@
 import Button from "./../Components/Button";
+import logo from "../Assets/shoppingCartLogo.svg";
 
 const Splash = ({setSplash}) => {
     return (
         <div className="bg-green h-screen flex flex-col justify-center">
+            <img src={logo} alt="GroceryList logo" className="bg-white shadow-2xl px-3 py-8 max-w-full w-[150px] self-center rounded-xl" />
             <h1 className="text-white text-4xl bold text-center m-4">Welcome to the GroceryList App!</h1>
             <p className="italic text-white text-center text-xl">Manage groceries with ease</p>
             <Button onClick={() => setSplash(false)}>Let's start!</Button>
